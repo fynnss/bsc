d53ef31b3ea1b7e029945585097235eae42d841b
Fynn <zcheng1004@gmail.com>
2023-10-11 17:47:27 +0800

    trie, core, eth: refactor rawdb write/delete/hasTrieNode

--- trie/sync.go
+++ trie/sync.go
@@ -27,6 +27,7 @@ import (
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethdb"
 	"github.com/ethereum/go-ethereum/log"
+	"github.com/ethereum/go-ethereum/trie/triedb"
 )
 
 // ErrNotRequested is returned by the trie sync when it's requested to process a
@@ -192,7 +193,7 @@ func (s *Sync) AddSubTrie(root common.Hash, path []byte, parent common.Hash, par
 		return
 	}
 	owner, inner := ResolvePath(path)
-	if rawdb.HasTrieNode(s.database, owner, inner, root, s.scheme) {
+	if triedb.HasTrieNode(s.database, owner, inner, root, s.scheme) {
 		return
 	}
 	// Assemble the new sub-trie sync request
@@ -350,7 +351,7 @@ func (s *Sync) Commit(dbw ethdb.Batch) error {
 	// Dump the membatch into a database dbw
 	for path, value := range s.membatch.nodes {
 		owner, inner := ResolvePath([]byte(path))
-		rawdb.WriteTrieNode(dbw, owner, inner, s.membatch.hashes[path], value, s.scheme)
+		triedb.WriteTrieNode(dbw, s.database, owner, inner, s.membatch.hashes[path], value, s.scheme)
 	}
 	for hash, value := range s.membatch.codes {
 		rawdb.WriteCode(dbw, hash, value)
@@ -475,7 +476,7 @@ func (s *Sync) children(req *nodeRequest, object node) ([]*nodeRequest, error) {
 					chash        = common.BytesToHash(node)
 					owner, inner = ResolvePath(child.path)
 				)
-				if rawdb.HasTrieNode(s.database, owner, inner, chash, s.scheme) {
+				if triedb.HasTrieNode(s.database, owner, inner, chash, s.scheme) {
 					return
 				}
 				// Locally unknown node, schedule for retrieval
